927dda6fc4f164da2e3ddc6f4d1da1212ee0354b
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-05-19 19:16:49 +0200

    First commit

---
+++ LVQ.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn import preprocessing, datasets, linear_model
+from sklearn.model_selection import train_test_split,KFold
+import seaborn as sns
