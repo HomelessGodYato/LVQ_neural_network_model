643d109bee5dc1d37ecbd31b8a9a8dd2b395e8bf
HomelessGodYato <yaroslavbanchenko@gmail.com>
2022-05-22 15:12:21 +0200

    Does not working

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
-from LVQ import *
+import math
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
 
 def main():
-    neural_network = LVQ()
-    data = neural_network.load_data('D:/desktop/Programming/Python/AI_ML/Neural networks/University Project/LVQ_NN/german_credit_data.csv')
-    print(data.head())
+    credit_data = pd.read_csv('D:/desktop/Programming/Python/AI_ML/Neural networks/University Project/LVQ_NN/german_credit_data.csv')
+    _class = credit_data['kredit']
+    del credit_data['Id']
+
 
 if __name__ == '__main__':
     main()
